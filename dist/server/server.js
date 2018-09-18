df58d92f256add91850d2ae282f4bd2e84e173a0
marvellous122 <marvellouspeak@gmail.com>
2018-09-17 19:02:58 -0700

    change ping logic

--- dist/server/server.js
+++ dist/server/server.js
@@ -14,11 +14,19 @@ wss.on('connection', (ws) => {
     extWs.isAlive = true;
     ws.on('pong', () => {
         extWs.isAlive = true;
+        console.log('--normalpong');
+        ws.send({
+            type: 'pong'
+        });
     });
     //connection is up, let's add a simple event
     ws.on('message', (msg) => {
         console.log('recevied %s', msg);
         const jMsg = JSON.parse(msg);
+        if (jMsg.type && jMsg.type === 'ping') {
+            console.log('---myping');
+            return;
+        }
         if (jMsg.msg === 'reconnecting') {
             //reconnected client, send all histories currently but we need some logic for data transfer
             ws.send(JSON.stringify({ msg: 'connected', history: chatHistories }));
@@ -56,16 +64,15 @@ wss.on('connection', (ws) => {
     });
 });
 // remove unconnected clients
-setInterval(() => {
-    wss.clients.forEach((ws) => {
-        const extWs = ws;
-        if (!extWs.isAlive)
-            return ws.terminate();
-        //check client is alive
-        extWs.isAlive = false;
-        ws.ping(null, undefined);
-    });
-}, 10000);
+// setInterval(() => {
+//     wss.clients.forEach((ws: WebSocket) => {
+//         const extWs = ws as ExtWebSocket;
+//         if (!extWs.isAlive) return ws.terminate();
+//         //check client is alive
+//         extWs.isAlive = false;
+//         ws.ping(null, undefined);
+//     });
+// }, 10000);
 //start our server
 server.listen(process.env.PORT || 8999, () => {
     console.log(`Server started on port ${server.address().port} :)`);
--- src/server.ts
+++ src/server.ts
@@ -26,12 +26,20 @@ wss.on('connection', (ws: WebSocket) => {
 
     ws.on('pong', () => {
         extWs.isAlive = true;
+        console.log('--normalpong');
+        ws.send({
+            type: 'pong'
+        });
     });
 
     //connection is up, let's add a simple event
     ws.on('message', (msg: string) => {
         console.log('recevied %s', msg);
         const jMsg = JSON.parse(msg);
+        if (jMsg.type && jMsg.type === 'ping') {
+            console.log('---myping');
+            return;
+        }
         if (jMsg.msg === 'reconnecting') {
             //reconnected client, send all histories currently but we need some logic for data transfer
             ws.send(JSON.stringify({msg: 'connected', history: chatHistories}));
@@ -71,17 +79,17 @@ wss.on('connection', (ws: WebSocket) => {
 
 
 // remove unconnected clients
-setInterval(() => {
-    wss.clients.forEach((ws: WebSocket) => {
+// setInterval(() => {
+//     wss.clients.forEach((ws: WebSocket) => {
 
-        const extWs = ws as ExtWebSocket;
+//         const extWs = ws as ExtWebSocket;
 
-        if (!extWs.isAlive) return ws.terminate();
-        //check client is alive
-        extWs.isAlive = false;
-        ws.ping(null, undefined);
-    });
-}, 10000);
+//         if (!extWs.isAlive) return ws.terminate();
+//         //check client is alive
+//         extWs.isAlive = false;
+//         ws.ping(null, undefined);
+//     });
+// }, 10000);
 
 //start our server
 server.listen(process.env.PORT || 8999, () => {
